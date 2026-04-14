9c831993eb22ecf2daafc8ec91961fba141f2295
Anjel <anjelradio@gmail.com>
2026-04-13 22:49:39 -0400

    test4

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -4,6 +4,25 @@ import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 
 export default async function Home() {
-  
+  const cookieStore = await cookies();
+    const token = cookieStore.get("access_token")?.value;
+  if (!token) {
+    redirect("/login");
+  }
+
+  const response = await schoolRepository.getSchoolsByUser();
+  if (!response.ok || !("data" in response) || !response.data) {
+    redirect("/inicio");
+  }
+
+  const schools = response.data;
+
+  if (schools.length === 0) {
+    redirect("/inicio");
+  }
+
+  // MVP: primer colegio
+
+  redirect(`/${schools[0].id}/inicio`);
   return <h1>HOME</h1>;
 }
